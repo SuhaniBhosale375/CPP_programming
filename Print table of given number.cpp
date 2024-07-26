62c3a59f26076fb520a96c091001ccd7f29104cc
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-07-26 13:09:05 +0530

    Cpp Programming

---
+++ Print table of given number.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Number
+{
+    public:
+        int iNo;
+
+        Number(int A)
+        {
+            iNo = A;
+        }
+
+        void Table()
+        {
+            int iCnt = 0;
+
+            cout<<"*******************************"<<"\n";
+
+            for(iCnt = 1; iCnt <= 10; iCnt++)
+            {
+                cout<<iNo*iCnt<<"\n\n";
+            }
+            cout<<"*******************************"<<"\n";
+        }
+};
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter any number :"<<"\n";
+    cin>>iValue;
+
+    Number Obj(iValue);
+
+    Obj.Table();
+
+    return 0;
+}
