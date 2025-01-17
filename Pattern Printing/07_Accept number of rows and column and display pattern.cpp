3d719e07272c3d50ffdb396cdc6c5be20acf9889
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-01-17 23:05:32 +0530

    Cpp Programming

---
+++ Pattern Printing/07_Accept number of rows and column and display pattern.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+/*
+    Row     6
+    Coloum  6
+
+    $   $   $   $   $   $
+    *   *   *   *   *   *
+    *   *   *   *   *   *
+    *   *   *   *   *   *
+    *   *   *   *   *   *
+    $   $   $   $   $   $ 
+
+   
+    
+*/
+
+
+
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+        Pattern()
+        {
+            iRow = 0;
+            iCol = 0;
+        }
+        Pattern(int a,int b)
+        {
+            iRow = a;
+            iCol = b;
+        }
+
+        void Display()
+        {
+            int i = 0, j = 0;
+
+            for(i = 1; i <= iRow; i++)
+            {    
+                for(j = 1; j <= iCol; j++)
+                {
+                if(i == 1 || i == iRow)
+                {
+                       cout<<"$"<<"\t";
+                }
+                else
+                {
+                       cout<<"*"<<"\t";
+                }
+                }
+               cout<<"\n"; 
+            }    
+        }
+};
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    cout<<"Enter Row number "<<"\n";
+    cin>>iValue1;
+
+    cout<<"Enter Row number "<<"\n";
+    cin>>iValue2;
+
+    Pattern obj(iValue1,iValue2);
+
+    obj.Display();
+
+    return 0;
+}
