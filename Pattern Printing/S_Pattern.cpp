1ec3a9671884a0e63c653c9cd2e62a85e98f41a9
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-01-18 22:47:00 +0530

    Cpp Programming

---
+++ Pattern Printing/S_Pattern.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+        Pattern()
+        {
+            iRow = 0;
+            iCol = 0;
+        }
+        Pattern(int a,int b)
+        {
+            iRow = a;
+            iCol = b;
+        }
+        void Display()
+        {
+            for(int i = 1; i <= iRow; i++)
+            {    
+                for(int j = 1; j <= iCol; j++)
+                {
+                    if(i == 1 || i == iCol || i == (iCol/2)+ 1 || (i > j  && j == 1 && i <= (iCol/2))|| (i < j && j == iCol && i > (iCol/2)))
+                    {
+                        cout<<" * ";
+                    }
+                    else
+                    {
+                        cout<<"   ";
+                    }
+                    
+                }
+                cout<<"\n"; 
+            }
+        }   
+};
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    cout<<"Enter Row number "<<"\n";
+    cin>>iValue1;
+
+    cout<<"Enter coloumn number "<<"\n";
+    cin>>iValue2;
+
+    Pattern obj(iValue1,iValue2);
+
+    obj.Display();
+
+    return 0;
+}
