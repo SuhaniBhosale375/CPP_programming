b02dd2804ec449e60e6e836d9eda8666a30ecffa
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-01-18 22:46:24 +0530

    Cpp Programming

---
+++ Pattern Printing/O_Pattern.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+        Pattern()
+        {
+            iRow = 0;
+            iCol = 0;
+        }
+        Pattern(int a,int b)
+        {
+            iRow = a;
+            iCol = b;
+        }
+        void Display()
+        {
+            for(int i = 1; i <= iRow; i++)
+            {    
+                for(int j = 1; j <= iCol; j++)
+                {
+                    if((j == 1 && i < iCol && i > 1) || (j == iCol  && i < iCol && i > 1) || (i == iCol && (j < iRow)&& (j > 1) || (i == 1) && (j < iRow)&& j > 1))
+                    {
+                        cout<<" * ";
+                    }
+                    else
+                    {
+                        cout<<"   ";
+                    }
+                    
+                }
+                cout<<"\n"; 
+            }
+        }   
+};
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    cout<<"Enter Row number "<<"\n";
+    cin>>iValue1;
+
+    cout<<"Enter coloumn number "<<"\n";
+    cin>>iValue2;
+
+    Pattern obj(iValue1,iValue2);
+
+    obj.Display();
+
+    return 0;
+}
